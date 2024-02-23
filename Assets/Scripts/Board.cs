cff1ec23adf2d41fdf07b208d64e0af51b5545d6
stavush <stavbenyamin@gmail.com>
2024-02-23 22:36:05 +0200

    fix: board init

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -43,42 +43,43 @@ public class Board : MonoBehaviour
 
     private void InitBoard()
     {
+        tiles = new Tile[columns, rows];
+
         for (int x = 0; x < columns; x++)
         {
             for (int y = 0; y < rows; y++)
             {
-                GameObject tileBackground = (GameObject)Instantiate(tileBGPrefab, new Vector3(x, y, 0), Quaternion.identity);
-            }
-        }
+                Vector3 position = new Vector3(x, y, 0);
 
-        tiles = new Tile[columns, rows];
-        for (int x=0;  x<columns; x++)
-        {
-            for(int y=0; y<rows; y++)
-            {
-                GameObject newTile = (GameObject)Instantiate(tilePrefabDict[TileType.NORMAL], new Vector3(x, y, 0), Quaternion.identity);
+                // Instantiate tiles background 
+                GameObject tileBackground = Instantiate(tileBGPrefab, position, Quaternion.identity);
+
+                // Instantiate tiles content
+                GameObject newTile = (GameObject)Instantiate(tilePrefabDict[TileType.NORMAL], position, Quaternion.identity);
                 newTile.name = "Tile(" + x + "," + y + ")";
                 newTile.transform.parent = transform;
 
-                tiles[x,y] = newTile.GetComponent<Tile>();
+                tiles[x, y] = newTile.GetComponent<Tile>();
                 tiles[x, y].Init(x, y, this, TileType.NORMAL);
 
                 if (tiles[x, y].IsMovable())
                 {
-                    tiles[x,y].MovableComponent.MoveTile(x,y);
+                    tiles[x, y].MovableComponent.MoveTile(x, y);
                 }
                 if (tiles[x, y].HasDesign())
                 {
-                    tiles[x, y].DesignComponent.SetDesign((TileDesign.DesignType)Random.Range(0, tiles[x,y].tileDesign.DesignSpritesAmount));
+                    tiles[x, y].DesignComponent.SetDesign((TileDesign.DesignType)Random.Range(0, tiles[x, y].tileDesign.DesignSpritesAmount));
                 }
+
             }
         }
     }
 
     public Vector2 GetPosition(int x, int y)
     {
-        return new Vector2(transform.position.x - columns / 2.0f + x, 
-            transform.position.y + rows / 2.0f - y);
+        return new Vector2(x, y);
+        //return new Vector2(transform.position.x - columns / 2.0f + x,
+            //transform.position.y + rows / 2.0f - y);
     }
 
     void Update()
